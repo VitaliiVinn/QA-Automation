dfe9fcaae942041286b39ac5272dd39b42fe9f44
Vitalii Vinnyk <vinvit82@gmail.com>
2023-02-09 23:14:48 +0200

    Open_Page

--- MavenProject/src/test/java/TestRunner.java
+++ MavenProject/src/test/java/TestRunner.java
@@ -1,16 +1,19 @@
 import com.hilel.core.driver.WebDriverFactory;
 import com.hilel.core.util.ConfigProvider;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import static com.hilel.core.util.WaitUtils.waitElementsUntilIsClickable;
 
 
 public class TestRunner {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","F:\\My documents\\Testing work\\chromedriver.exe");
-        System.setProperty("webdriver.opera.driver","F:\\My documents\\Testing work\\operadriver.exe");
-        System.setProperty("webdriver.edge.driver","F:\\My documents\\Testing work\\msedgedriver.exe");
-        System.setProperty("webdriver.ie.driver","F:\\My documents\\Testing work\\IEDriverServer.exe");
         WebDriver driver = WebDriverFactory.getDriver();
         driver.get(ConfigProvider.BASE_URL);
+        WebElement buttonCourses = driver.findElement(By.xpath("(//p[@class='course-cat-bar_label'])[2]"));
+        waitElementsUntilIsClickable(driver, buttonCourses);
+        buttonCourses.click();
         driver.quit();
     }
 }
