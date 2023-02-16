f0afdff662bc59b4cc8e1e2824313e02ae03d30d
Vitalii Vinnyk <vinvit82@gmail.com>
2023-02-16 18:37:42 +0200

    homeWork14

--- Automation/src/test/java/TestRunner.java
+++ Automation/src/test/java/TestRunner.java
@@ -4,15 +4,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import static com.hillel.util.core.WaitUtils.waitElementsUntilIsClickable;
+import static com.hillel.util.core.Wrapper.*;
 
 public class TestRunner {
     public static void main(String[] args) {
         WebDriver driver = WebDriverFactory.getDriver();
         driver.get(ConfigProvider.BASE_URL);
         WebElement buttonCourses = driver.findElement(By.xpath("(//div[@class='course-bar_body'])[3]"));
-        waitElementsUntilIsClickable(driver, buttonCourses);
+//        waitElementsUntilIsClickable(driver, buttonCourses);
+//        waitElementsUntilIsVisible(driver, buttonCourses);
+        clickSleep(driver, buttonCourses);
+        clickHomePageLogo(driver, buttonCourses);
+        isClickable(driver, buttonCourses);
         buttonCourses.click();
-//        driver.quit();
+        driver.quit();
     }
 }
